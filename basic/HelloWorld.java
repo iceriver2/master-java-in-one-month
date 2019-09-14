9f4c3f7db9b8bb2c28495325ebbda0747bf77a66
Iceman <31529857@qq.com>
2019-09-14 15:51:28 +0800

    增加helloworld类说明

--- basic/HelloWorld.java
+++ basic/HelloWorld.java
@@ -1,7 +1,15 @@
+/**
+ * HelloWorld 类名必须与文件名相同
+ * 
+ * 编译 javac HelloWorld.java
+ * 执行 java HelloWorld
+ */
+
 public class HelloWorld
 {
-    public static void main(String args[]) 
+    /* main()函数是每个应用的入口点 */
+    public static void main(String args[]) // args 将输入视为字符串数组
     {
-        System.out.println("Hello World"); 
+        System.out.println("Hello World"); // 调用系统包打印
     }
 }
