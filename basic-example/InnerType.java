ac05342d46743ee416abcb6c25e3909034e18902
Iceman <31529857@qq.com>
2019-09-21 16:09:42 +0800

    非静态成员类型的用法

--- basic-example/InnerType.java
+++ basic-example/InnerType.java
@@ -7,10 +7,15 @@ public class InnerType
 {
     public static void main(String[] args) {
         Outter ot = new Outter("Hello");
-        out.println(ot.uname());
+        out.println("Outter name: " + ot.uname());
+
+        Outter.Inner in = ot.new Inner(); // 注意这个 new 的用法
+        in.print();
 
         B b = new B();
-        out.println(b.iname());
+        out.println("B name: " + b.iname());
+
+        ot.print();
     }
 }
 
@@ -23,14 +28,40 @@ class Outter
         }
     }
 
-    private static String type = "Outter"; //顶层类的私有静态成员
+    // 非静态成员接口
+    public class Inner {
+        private String iname = "";
+        public Inner(String n) {
+            iname = n;
+        }
+        public Inner() {
+            iname = Outter.this.name; // 用外层类的name复制，在没有重名的情况也可以直接使用name
+        }
+        public String getName() {
+            return iname;
+        }
+        public void print() {
+            out.println("Inner says: " + iname);
+        }
+    }
+
+    public static String type = "Outter"; //顶层类的私有静态成员
 
     private String name;
+    private Inner mi; // 内部创建一个非静态成员实例
     public Outter(String n) {
         name = n;
+        mi = new Inner(name);
+    }
+    public Outter() {
+        name = "Unknown";
+        mi = new Inner(name);
     }
     public String uname() {
         return name;
+    }
+    public void print() {
+        out.println("2 exists: " + name + " " + mi.getName());
     }
 }
 class B implements Outter.If { // 实现If接口
